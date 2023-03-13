66d5ce0490a7e7f6d6bcfa5f206e950357ef5ff3
PunhanAliyeva <eliyevapunhan567@gmail.com>
2023-03-14 03:30:53 +0400

    first task

---
+++ tasks.js
@@ -0,0 +1,10 @@
+let input=document.querySelector("input");
+let showStudent=document.querySelectorAll("tr");
+let submit=document.querySelector("Submit");
+submit.addEventListener("click",function(){
+    showStudent.innerHTML+=<tr>name</tr>;
+    showStudent.innerHTML+=<tr>surname</tr>;
+    showStudent.innerHTML+=<tr>age</tr>;
+    showStudent.innerHTML+=<tr>sexuality</tr>;
+    showStudent.innerHTML+=<tr>mail</tr>;
+});
